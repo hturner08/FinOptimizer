84b26c097a11f242f0890efc971efbecb49293e4
Herbert M. Turner <herbieturner@verizon.net>
2018-09-30 16:24:18 -0400

    Currently working on access simulation data

--- FinOptimizerSimulationListener.java
+++ FinOptimizerSimulationListener.java
@@ -1,8 +1,21 @@
 package net.sf.openrocket.FinOptimizer;
 
+import net.sf.openrocket.document.Simulation;
+
+import net.sf.openrocket.gui.simulation.SimulationPlotPanel;
+
 import net.sf.openrocket.simulation.SimulationStatus;
 import net.sf.openrocket.simulation.exception.SimulationException;
 import net.sf.openrocket.simulation.listeners.AbstractSimulationListener;
+import net.sf.openrocket.simulation.FlightDataBranch;
+import net.sf.openrocket.simulation.FlightDataType;
+import net.sf.openrocket.simulation.FlightEvent;
+
+import net.sf.openrocket.unit.Unit;
+import net.sf.openrocket.unit.UnitGroup;
+
+import net.sf.openrocket.util.LinearInterpolator;
+
 
 public class FinOptimizerSimulationListener extends AbstractSimulationListener {
 
@@ -15,7 +28,7 @@ public class FinOptimizerSimulationListener extends AbstractSimulationListener {
   }
 
   @Override
-  public int[] Optimizer(SimulationStatus status, double[] initial_params) throws SimulationException {
+  public int[] Optimizer(Simulation simulation, SimulationStatus status, double[] initial_params) throws SimulationException {
     double[] final_params = initial_params;
     for(int i = 0; i <11;i++){
 
